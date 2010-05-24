0c39a140dafc639da1408724f5b12e253bac91b2
Ryan McGrew <ryanmcgrew@gmail.com>
2010-05-24 13:40:36 -0500

    adding symfony to the project

---
+++ lib/plugins/sfPropelPlugin/data/generator/sfPropelForm/default/template/sfPropelFormBaseTemplate.php
@@ -0,0 +1,15 @@
+[?php
+
+/**
+ * Project form base class.
+ *
+ * @package    brat_or_not
+ * @subpackage form
+ * @author     Your name here
+ */
+abstract class BaseFormPropel extends sfFormPropel
+{
+  public function setup()
+  {
+  }
+}
